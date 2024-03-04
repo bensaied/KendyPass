fedf641db5446ae0fddf193c5a76db908f30ffa0
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-03-04 19:28:32 +0100

    Upload(backend): Source Code

---
+++ constants.js
@@ -0,0 +1,31 @@
+<<<<<<< HEAD
+const Role = {
+  ADMIN: "ADMIN" ,
+  USER: "USER",
+};
+const Status = {
+  ACTIVE: "ACTIVE",
+  INACTIVE: "INACTIVE",
+  STANDBY: "STANDBY"
+};
+
+module.exports = {
+  Role,
+  Status,
+};
+=======
+const Role = {
+  ADMIN: "ADMIN" ,
+  USER: "USER",
+};
+const Status = {
+  ACTIVE: "ACTIVE",
+  INACTIVE: "INACTIVE",
+  STANDBY: "STANDBY"
+};
+
+module.exports = {
+  Role,
+  Status,
+};
+>>>>>>> 8764759c10e35b6a79265eefde7644382b99caa7
