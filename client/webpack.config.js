1c6d51e5545b20876b566075b41c6ed068912d57
Oussama_Ben_Saied <saayeed@outlook.fr>
2024-03-04 19:36:12 +0100

    Upload(frontend): Add packages and config files

---
+++ client/webpack.config.js
@@ -0,0 +1,24 @@
+/*module.exports = {
+    entry: './app/app.jsx',
+    output: {
+        path : './app',
+        filename: 'bundle.js',
+    },
+    cache : true,
+    watch : true,
+    devtool: "eval",
+    module: {
+        loaders: [
+            {
+                test: /\.jsx$/,
+                loader: 'jsx-loader?harmony'
+            }
+        ]
+    },
+    resolve : {
+        extensions : ['', '.js', '.jsx']
+    },
+    //target: 'node',
+    node:'empty'
+      
+};*/
